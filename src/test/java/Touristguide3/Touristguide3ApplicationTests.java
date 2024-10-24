a053c08f20692fb58cb7ce0d0d289b50e93b3c43
ayan786001 <mf786001@gmail.com>
2024-10-24 16:49:22 +0200

    Done

--- src/test/java/Touristguide3/Touristguide3ApplicationTests.java
+++ src/test/java/Touristguide3/Touristguide3ApplicationTests.java
@@ -1,9 +1,9 @@
 package Touristguide3;
 
+import kea.ayan.touristguide3.Touristguide3Application;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
+@SpringBootTest(classes = Touristguide3Application.class)
 class Touristguide3ApplicationTests {
 
     @Test
